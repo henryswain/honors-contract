16f2eb41bf2be1db0016446eb1c28dfef96fac26
henryswain <henryswain3141@gmail.com>
2025-07-21 13:30:21 -0500

    on Main branch:
    - forgot to include bar chart component in previous commit

--- src/components/BarChart_Sleep_And_SocialMedia.vue
+++ src/components/BarChart_Sleep_And_SocialMedia.vue
@@ -2,8 +2,41 @@
     // get basics from chart.js
     import { Chart } from 'chart.js/auto';
 
-    // we need onMounted and onUpdated from Vue
-    import { onMounted, onUpdated } from 'vue';
+    import { onMounted, onUpdated, ref, nextTick, computed} from 'vue';
+
+    const screenSize = ref("")
+    window.addEventListener('resize', handleScreenChange)
+
+
+function handleScreenChange() {
+    if (window.innerWidth <= 480) {
+        screenSize.value = "x-small"
+    }
+    else if (window.innerWidth >= 481 && window.innerWidth <= 768) {
+        screenSize.value = "small";
+    } else if (window.innerWidth > 768 && window.innerWidth <= 1279) {
+        screenSize.value = "medium";
+    } else {
+        screenSize.value = "large";
+    }
+    
+    nextTick();
+    
+    // Update chart if it exists
+    if (chartObj) {
+        // Update font sizes
+        console.log("screenSize.value: ", screenSize.value)
+        chartObj.options.scales.y.title.font.size = (screenSize.value === "x-small") ? 12 : ((screenSize.value === "small") ? 16 : (screenSize.value === "medium" ? 20 :24));
+        chartObj.options.scales.y.title.text = (screenSize.value == "large") ? "percentage or partipants" : ["percentage of ", "participants"]
+        chartObj.options.scales.x.title.font.size = (screenSize.value === "x-small") ? 12 : ((screenSize.value === "small") ? 16 : (screenSize.value === "medium" ? 24 : 28));
+        chartObj.options.scales.x.ticks.font.size = (screenSize.value === "x-small") ? 8 : ((screenSize.value === "small") ? 12 : (screenSize.value === "medium" ? 16 : 20));
+        chartObj.options.plugins.title.font.size = (screenSize.value === "x-small") ? 12 : ((screenSize.value === "small") ? 16 : (screenSize.value === "medium" ? 24 : 28));
+        
+        // Re-render the chart
+        chartObj.update();
+    }
+}
+
 
     // define the data coming into the component
     const props = defineProps({
@@ -18,6 +51,7 @@
     let chartObj;
 
     async function createChart() {
+        console.log("createChart called")
         // creates chart based on props
         // see full docs here: https://www.chartjs.org/docs/latest/charts/line.html
         chartObj = new Chart(
@@ -33,9 +67,9 @@
                         y: {
                             title: {
                                 display: true,
-                                text: "percentage of participants",
+                                text: (screenSize.value == "small" || screenSize.value == "x-small") ? ["percentage of ", "participants"] : "percentage of participants",
                                 font: {
-                                    size: 36
+                                    size: 24
                                 }
                             },
                             min: 0,
@@ -46,12 +80,17 @@
                                 display: true,
                                 text: `${props.chartLabel} categories of participants with ${props.amount}`,
                                 font: {
-                                    size: 36
+                                    size: 24
+                                    // size: (screenSize.value == "small") ? 9 : ((screenSize.value == "medum") ? 16 : 24)
                                 }
                             },
                             ticks: {
+                                autoSkip: false,
+                                // maxRotation: ((screenSize.value == "x-small") ? 90 : undefined),
+                                // minRotation: ((screenSize.value == "small" || screenSize.value == "x-small") ? 50 : undefined),
                                 font: {
                                     size: 12
+                                    // size: (screenSize.value == "small") ? 4 : ((screenSize.value == "medum") ? 6 : 12)
                                 }
                             }
                         },
@@ -69,6 +108,13 @@
                         }
                     },
                     plugins: {
+                        tooltip: {
+                            callbacks: {
+                                title: (context) => {
+                                    return context[0].label.replaceAll(",", " ")
+                                }
+                            }
+                        },
                         legend: {
                             display: false
                         },
@@ -77,6 +123,7 @@
                             text: `Amount of ${props.chartLabel} when using social media for ${props.amount}`,
                             font: {
                                 size: 40
+                                // size: (screenSize.value == "small") ? 20 : ((screenSize.value == "medum") ? 30 : 40)
                             }
                         },
                         datalabels: {
@@ -97,6 +144,8 @@
 
     // when the page loads, we create a chart
     onMounted(() => {
+            // Initial check
+        handleScreenChange();
         // calls asynchronous function to create chart
         createChart();
     }        
@@ -116,12 +165,25 @@
         // calls asynchronous function to update chart
         updateChart();
     }        
-    );
+    )
+
 </script>
 
+<style>
+.chart-container {
+  position: relative;
+  margin: auto;
+  height: 80vh;
+  width: 80vw;
+  max-width: 100%;
+}
+</style>
 <template>
-    <div class="container">
+    <!-- <div class="container"> -->
         <!-- matches id used in createChart() -->
-        <canvas id="thisChart"></canvas>
-    </div>
+
+        <div class="chart-container">
+            <canvas id="thisChart"></canvas>
+        </div>
+    <!-- </div> -->
 </template>
